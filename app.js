653f766254df61612e221f9ca9390f964d05fe1b
DrRawley <rawley.greene@gmail.com>
2023-12-05 22:17:03 -0500

    added views dir, home.ejs, and set route to home

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs'); //set default view engine to ejs
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(3000, () => {
     console.log("Listening on port 3000...");
 });
 
+//Basic route for initial testing
 app.get('/', (req, res) => {
-    res.send('<h1>YelpCamp!</h1><h3>\"This works, apparently.\" --Future Crew</h3>');
+    //res.send('<h1>YelpCamp!</h1><h3>\"This works, apparently.\" --Future Crew</h3>');
+    res.render('home');
 });
