279e747b4431b96a82227cd70f16e04a9f693dd2
DrRawley <rawley.greene@gmail.com>
2023-12-05 22:31:08 -0500

    setup db connection.  added campground schema

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+// The following mongoose options are no longer needed
+// {   useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true}
+
+const db = mongoose.connection; //db is just a shortcut to avoid typing mongoose.connection all the time
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected.");
+});
 
 app.set('view engine', 'ejs'); //set default view engine to ejs
 app.set('views', path.join(__dirname, 'views'));
