fe9a09a5241c953bb2efe3759bb869f925e12bb5
DrRawley <rawley.greene@gmail.com>
2023-12-05 23:19:55 -0500

    seeds/index.js programmed seed logic

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,9 @@
+//Load in base directory, execute from inside node: ".load seeds/index.js"
+
 const mongoose = require('mongoose');
 const Campground = require('./models/campground'); //Include model
+const cities = require('./seeds/cities');
+const { descriptors, places } = require('./seeds/seedHelpers');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -9,6 +13,23 @@ db.once("open", () => {
     console.log("Database connected.");
 });
 
-const seedDB = async () => {
+//Load in base directory, execute from shell
+const deleteWholeDB = async () => {
     await Campground.deleteMany({}); //delete everything to make good fake database
+};
+
+//Quick function to get a random element from an array
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+//Seed database using cities and seedHelpers
+const seedDB = async () => {
+    for (let i = 0; i < 50; i++) {
+        const rand1000 = Math.floor(Math.random() * 1000);
+
+        const camp = new Campground({
+            location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
 };
