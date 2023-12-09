89b4d599e483422317e2dc4d37662f683bb0ffe9
DrRawley <rawley.greene@gmail.com>
2023-12-08 23:05:26 -0500

    error template

--- app.js
+++ app.js
@@ -94,8 +94,9 @@ app.all('*', (req, res, next) => {
 //Set up error handlers
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500, message = 'Something went wrong.' } = err;
-    res.status(statusCode).send(message);
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something went wrong.';
+    res.status(statusCode).render('error', { err });
 
     //next(err);
 })
