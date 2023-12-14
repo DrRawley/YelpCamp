3e0e16cb09bd1c0930e9bbf58936540483d96aea
DrRawley <rawley.greene@gmail.com>
2023-12-13 22:02:19 -0500

    registration handling setup

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
-router.post('/register', async (req, res) => {
-    res.send(req.body)
-});
+router.post('/register', catchAsync(async (req, res) => {
+    const { email, username, password } = req.body;
+    const user = new User({ email, username });
+    try {
+        const registeredUser = await User.register(user, password);
+    } catch (e) {
+        console.log(e.message);
+        req.flash('error', `E-mail already exists!`);
+        return res.redirect('/register');
+    }
+
+    console.log(registeredUser);
+    req.flash('success', 'Welcome to YelpCamp!');
+    res.redirect('/campgrounds');
+}));
 
 
 
