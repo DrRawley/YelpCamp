9e0812e22ec5e70a65744ab57ccd7a17b0d982ec
DrRawley <rawley.greene@gmail.com>
2023-12-18 14:09:09 -0500

    set up dotenv

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") { //For reading development .env keys
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const session = require('express-session');
