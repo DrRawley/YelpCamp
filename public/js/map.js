048d1a8072a20fe68bd596093ab499432d9c4475
DrRawley <rawley.greene@gmail.com>
2023-12-19 00:11:33 -0500

    add map popup and customize

--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,16 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    style: 'mapbox://styles/mapbox/light-v10', // style URL
     center: [long, lat], // starting position [lng, lat]
     zoom: 9, // starting zoom
 });
 new mapboxgl.Marker()
     .setLngLat([long, lat])
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campgroundTitle}</h3>`
+            )
+    )
     .addTo(map);
