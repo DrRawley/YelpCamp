3a0e5b684242c44c33c7b2fe78dec49e19e8ffc9
DrRawley <rawley.greene@gmail.com>
2023-12-22 13:40:25 -0500

    add port env variable

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ passport.deserializeUser(User.deserializeUser());
 
 
 // ********* Start Express Listening ************                                    
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Listening on port 3000...");
 });
 
